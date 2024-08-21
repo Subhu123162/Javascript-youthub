0617c7a83eb132e50b346e98904e8e06005ebd27
Subhu123162 <subasishjana36@gmail.com>
2024-08-21 04:44:14 +0000

    datatypes

---
+++ 01_basiscs/datatype.js
@@ -0,0 +1,30 @@
+"use strict";//treat all js code as never version
+
+// alert(3+3) we are using node js not browser
+
+console.log(4+4); //code readability be high
+console.log("subhu");
+
+let name = "subhu" //strings data type
+let age = 20 //number ....
+let isLoggedIn = true //boolen
+let state = null
+
+
+
+//number => 2 to powr 53
+//bigint =>
+// string=>""
+//boolen=>true/false
+//null =>standalon value
+//undefine =>
+//symbol = unique 
+
+//object
+console.log(typeof "hasi");
+console.log(typeof 30);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
