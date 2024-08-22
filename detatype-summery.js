8ea8869a68b13556dbdfc461cb6b749ab2798cfc
Subhu123162 <subasishjana36@gmail.com>
2024-08-22 03:09:36 +0000

    data type summary

---
+++ detatype-summery.js
@@ -0,0 +1,45 @@
+// ** primitive
+
+
+// 7 types:- string,number,boolen,null,undefined,symbol,bigInt
+
+
+//javascript language is a dynamic type not a static....because variable can hold values of defurent types during run time ***
+const score = 100
+const scorevalue = 100.3
+
+
+let useremail;
+
+const id  = Symbol('237')
+const anetherId = Symbol('237')
+
+
+console.log(id == anetherId);  //false
+
+const bignumber = 4857943579784n
+
+
+
+
+
+//referance (non premitive)
+
+
+//array,objects,functions
+
+const heros = ["sjk",'suh','iis']
+
+let myobj = {
+    name: "subhu",
+    age : 22,
+}
+
+const myfunction = function(){
+    console.log("hellow world");
+    
+}
+
+
+console.log(typeof heros);
+
